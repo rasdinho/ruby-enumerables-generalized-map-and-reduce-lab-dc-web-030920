a207b574bd0d51ef27a4ac5fa22c28fa5732f36f
Ras azzab <ras.dinho7@gmail.com>
2020-02-28 18:06:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,18 @@ def map(array)
   end 
   new
 end
+
+def reduce(array, sv=nil)
+  if sv 
+    sum = sv 
+    i = 0 
+  else 
+    sum = array[0]
+    i = 1 
+  end 
+  while i < array.length 
+  sum = yield(sum, array[i])
+  i += 1 
+end 
+sum
+end
